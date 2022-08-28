8ffbc40173803713d8b137e6bce93cb887c35e84
rachelfff <racheligenut121@gmail.com>
2022-08-28 19:14:13 +0300

    add length

--- exam/src/app/modules/gallery-images/components/images-scroller/images-scroller.component.ts
+++ exam/src/app/modules/gallery-images/components/images-scroller/images-scroller.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { GalleryImagesService } from '../../services/gallery-images.service';
 import {Image} from '../../models/image';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'images-scroller',
